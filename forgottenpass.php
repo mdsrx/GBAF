695ff9c2bf1623ea820e05bbb8f227c0dbe8f41b
mdsrx <desrousseaux.ma@gmail.com>
2021-01-02 19:34:41 +0100

    Ajout div "content-page"

--- forgottenpass.php
+++ forgottenpass.php
@@ -14,17 +14,19 @@
 	<?php
 	include 'header.php';
 	?>
-	<div class="container">
-		<div class="bloc-content">
-			<!-- Connexion -->
-			<h2>Mot de passe oublié</h2>
-			<form method="post" action="forgottenpass.php">
-				<p>
-					<label for="username">Username</label>
-					<input type="text" name="username" maxlength="255" required />
-				</p>
-				<input type="submit" name="connexion" class="button" value="Afficher ma question secrète >" />
-			</form>			
+	<div class="content-page">
+		<div class="container">
+			<div class="bloc-content">
+				<!-- Connexion -->
+				<h2>Mot de passe oublié</h2>
+				<form method="post" action="forgottenpass.php">
+					<p>
+						<label for="username">Username</label>
+						<input type="text" name="username" maxlength="255" required />
+					</p>
+					<input type="submit" name="connexion" class="button" value="Afficher ma question secrète >" />
+				</form>			
+			</div>
 		</div>
 	</div>
 	<?php
